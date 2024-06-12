392e027bbba6e048f44ab1c6b70199bb18d2609d
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-06-12 09:58:47 +0530

    initial

--- src/Components/StudentList.jsx
+++ src/Components/StudentList.jsx
@@ -48,7 +48,7 @@ const StudentList = () => {
     {
       id:6,
       img : 'austin-wade-X6Uj51n5CE8-unsplash 1.png',
-      name : 'Firstname Lastname',
+      name : 'Firstname',
       danceForm : 'Ballet',
       joiningDate : '30th July, 2021',
       feeStatus : 'Paid'
@@ -64,7 +64,7 @@ const StudentList = () => {
     {
       id:8,
       img : 'austin-wade-ex6qfO4TPMY-unsplash 1.png',
-      name : 'Firstname Lastname',
+      name : 'Firstname',
       danceForm : 'Contemporary',
       joiningDate : '30th July, 2021',
       feeStatus : 'Paid'
@@ -72,7 +72,7 @@ const StudentList = () => {
     {
       id:9,
       img : 'vince-fleming-j3lf-Jn6deo-unsplash 1.png',
-      name : 'Firstname Lastname',
+      name : 'Firstname',
       danceForm : 'Ballet',
       joiningDate : '31th August, 2020',
       feeStatus : 'Paid'
@@ -81,20 +81,20 @@ const StudentList = () => {
 
   return (
     <div>   
-   <div className={`py-4 sm:px-5 px-3  flex-1 h-screen max-w-full `}>
+   <div className={`py-4 sm:px-5 px-2  flex-1 h-screen max-w-full`}>
 
         <h1 className='shadow py-3 pl-5 lg:rounded-full rounded text-[18px] sm:text-2xl max-w-[100%] font-semibold'>Student List</h1>
 
       <div className='max-w-[100%] grid sm:grid-cols-2 grid-cols-1 justify-between  my-5 items-center p-4 shadow-md mb-5'>
 
-        <div className='w-[100%]'>
+        <div className='max-w-[100%]'>
 
-          <h1 className='text-4xl font-bold text-[#CE00FF]'>143 <span className='sm:text-[15px] text-[#A686AD] text-[13px'>Students</span></h1>
+          <h1 className='md:text-4xl text-2xl font-bold text-[#CE00FF]'>143 <span className='sm:text-[15px] text-[#A686AD] text-[13px'>Students</span></h1>
 
         </div>
-        <div className='text-[#A686AD]  flex items-center justify-end'>
+        <div className='text-[#A686AD]  flex items-center my-5 sm:my-0 md:justify-end'>
           <span className='font-semibold mr-5'>Sort By:</span>
-          <select className='shadow outline-none py-2 px-4 rounded '>
+          <select className='shadow outline-none py-2 px-2 rounded '>
           {Dances.map((value,index) => {
                   return(
               <option value={value} key={index} className='font-semibold py-3 '>{value}</option>
@@ -106,7 +106,7 @@ const StudentList = () => {
 
         <div className='max-w-full mb-10 overflow-x-auto'>
 
-          <table className='min-w-full border-none '>
+          <table className='w-full border-none '>
             <thead className='hidden sm:table-header-group'>
               <tr className='shadow-lg text-[#A686AD]'>
                 <th className='py-1'>Student</th>
@@ -119,10 +119,10 @@ const StudentList = () => {
               {StudentData.map((value,index) => {
                 return(
                 <tr className='shadow-lg my-5' key={index} >
-                <td className='py-3 sm:flex block items-center '><img src={value.img} className='mr-2' alt="studentimg" /> <span className='font-semibold my-3'>{value.name}</span></td>
-                <td className='text-[#A686AD] font-medium'>{value.danceForm}</td>
-                <td className='text-[#A686AD] font-medium'>{value.joiningDate}</td>
-                <td className={`${value.feeStatus ===  'Paid' ? 'text-[#04BC00]' : 'text-[#A686AD] '} font-medium`}>{value.feeStatus}</td>
+                <td className='py-3 sm:flex block items-center '><img src={value.img} className='sm:mr-2 h-8 sm:h-10' alt="studentimg" /> <span className='font-semibold my-3 text-[15px] md:text-[18px]'>{value.name}</span></td>
+                <td className='text-[#A686AD] font-medium text-[15px] md:text-[18px]'>{value.danceForm}</td>
+                <td className='text-[#A686AD] font-medium text-[15px] md:text-[18px]'>{value.joiningDate}</td>
+                <td className={`${value.feeStatus ===  'Paid' ? 'text-[#04BC00]' : 'text-[#A686AD] '} font-medium text-[15px] md:text-[18px]`}>{value.feeStatus}</td>
               </tr>
                 )
               })}
@@ -137,136 +137,3 @@ const StudentList = () => {
 }
 
 export default StudentList
-
-
-
-
-
-
-
-// import React from 'react';
-
-// const StudentList = () => {
-//   const Dances = ['Dance From', 'Hip-Hop', 'Contemporary', 'Bharatanatyam', 'Ballet', 'Lorem', 'Epsum', 'Dolor', 'Lorem'];
-
-//   const StudentData = [
-//         {
-//           id:1,
-//           img : 'jake-nackos-IF9TK5Uy-KI-unsplash 1.png',
-//           name : 'Mirabel',
-//           danceForm : 'Ballet',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:2,
-//           img : 'vince-fleming-j3lf-Jn6deo-unsplash 1 (1).png',
-//           name : 'Zachary Erza',
-//           danceForm : 'Ballet',
-//           joiningDate : '31th August, 2020',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:3,
-//           img : 'austin-wade-ex6qfO4TPMY-unsplash 1.png',
-//           name : 'Love Rawlins',
-//           danceForm : 'Bharatanatyam',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Pending'
-//         },
-//         {
-//           id:4,
-//           img : 'shannon-vandenheuvel-Z9p4M58Hdyo-unsplash 1.png',
-//           name : 'Katrina Hawkins',
-//           danceForm : 'Ballet',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:5,
-//           img : 'austin-wade-X6Uj51n5CE8-unsplash 1.png',
-//           name : 'Dorian',
-//           danceForm : 'Ballet',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:6,
-//           img : 'austin-wade-X6Uj51n5CE8-unsplash 1.png',
-//           name : 'Firstname Lastname',
-//           danceForm : 'Ballet',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:7,
-//           img : 'austin-wade-X6Uj51n5CE8-unsplash 1.png',
-//           name : 'Hamnet Piercer',
-//           danceForm : 'Contemporary',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Pending'
-//         },
-//         {
-//           id:8,
-//           img : 'austin-wade-ex6qfO4TPMY-unsplash 1.png',
-//           name : 'Firstname Lastname',
-//           danceForm : 'Contemporary',
-//           joiningDate : '30th July, 2021',
-//           feeStatus : 'Paid'
-//         },
-//         {
-//           id:9,
-//           img : 'vince-fleming-j3lf-Jn6deo-unsplash 1.png',
-//           name : 'Firstname Lastname',
-//           danceForm : 'Ballet',
-//           joiningDate : '31th August, 2020',
-//           feeStatus : 'Paid'
-//         },
-//       ]
-
-//   return (
-//     <div className="py-4 sm:px-5 px-3 flex-1 h-screen w-full">
-//       <h1 className="shadow py-3 pl-5 lg:rounded-full rounded text-[18px] sm:text-2xl w-full font-semibold">Student List</h1>
-//       <div className="w-full grid sm:grid-cols-2 grid-cols-1 justify-between my-5 items-center p-4 shadow-md mb-5">
-//         <div className="w-full">
-//           <h1 className="text-4xl font-bold text-[#CE00FF]">143 <span className="sm:text-[15px] text-[#A686AD] text-[13px]">Students</span></h1>
-//         </div>
-//         <div className="text-[#A686AD] flex items-center justify-end">
-//           <span className="font-semibold mr-5">Sort By:</span>
-//           <select className="shadow outline-none py-2 px-4 rounded">
-//             {Dances.map((value, index) => (
-//               <option value={value} key={index} className="font-semibold py-3 ">{value}</option>
-//             ))}
-//           </select>
-//         </div>
-//       </div>
-//       <div className="w-full overflow-x-auto">
-//         <table className="min-w-full border-none">
-//           <thead className="hidden sm:table-header-group">
-//             <tr className="shadow-lg">
-//               <th className="py-1">Student</th>
-//               <th>Dance Form</th>
-//               <th>Joining Date</th>
-//               <th>Fee Status</th>
-//             </tr>
-//           </thead>
-//           <tbody className="block sm:table-row-group">
-//             {StudentData.map((value, index) => (
-//               <tr className="shadow-lg my-5 block sm:table-row" key={index}>
-//                 <td className="py-3 flex items-center block sm:table-cell">
-//                   <img src={value.img} className="mr-3" alt="studentimg" />
-//                   <span className="font-semibold my-3">{value.name}</span>
-//                 </td>
-//                 <td className="block sm:table-cell">{value.danceForm}</td>
-//                 <td className="block sm:table-cell">{value.joiningDate}</td>
-//                 <td className={`block sm:table-cell ${value.feeStatus === 'Paid' ? 'text-green-500' : ''}`}>{value.feeStatus}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default StudentList;
