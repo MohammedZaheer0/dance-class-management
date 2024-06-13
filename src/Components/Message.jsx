39906ed6dbd55024a22cdb1b010f5e7f0fb2b584
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-06-13 14:37:02 +0530

    initial

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -51,7 +51,7 @@ const Message = () => {
 
   return (
     <div>
-      <div className={`py-4 sm:px-5 px-3  flex-1 h-screen max-w-full `}>
+      <div className={`py-4 sm:px-5 px-3  flex-1 h-auto max-w-full `}>
 
       <h1 className='shadow py-3 pl-5 lg:rounded-full rounded text-[18px] sm:text-2xl max-w-[100%] font-semibold'>Message</h1>
 
